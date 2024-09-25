530f949e40503fedccf954fc7beebb9747f392a3
JacMcQ <jacqlyn.mcquade@icloud.com>
2024-09-24 21:04:52 -0600

    debug for file path for index.hml

--- server/server.js
+++ server/server.js
@@ -2,12 +2,30 @@ const express = require('express');
 const path = require("path");
 
 const app = express();
+
+// Serve static files from the client/dist directory
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Define a route to serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
+// Start the server
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Now listening on port: ${PORT}`);
+});
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+
+
+
+
+
+
+
+
+
+
